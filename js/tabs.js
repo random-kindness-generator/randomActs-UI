07da4da826b23645453183079388c27c0e6bdd66
hcopasstarke <hcopasstarke@gmail.com>
2019-05-30 11:33:03 -0700

    updates?

---
+++ js/tabs.js
@@ -0,0 +1,34 @@
+class TabLink {
+    constructor(element) { 
+        this.element = element; 
+        this.data = this.element.dataset.tab; 
+        this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`); 
+        this.tabItem = new TabItem(this.itemElement); 
+        this.element.addEventListener('click', () => this.select(event)); 
+    };
+
+    select(event) {
+        let links1 = document.querySelectorAll('.tabs-link');
+        links1 = Array.from(links1); 
+        for (let i = 0; i < links1.length; i++) {
+            links1[i].classList.remove('tabs-link-selected');
+        }
+        this.element.classList.add('tabs-link-selected'); // #13  
+        this.tabItem.select(); 
+    }
+}
+
+class TabItem { 
+    constructor(element) { 
+        this.element = element;
+    }
+    select(event) {
+        let items = document.querySelectorAll('.tabs-item');
+        items = Array.from(items).forEach(item => item.classList.remove('tabs-item-selected')); 
+        this.element.classList.add('tabs-item-selected'); 
+    }
+}
+
+let links = document.querySelectorAll('.tabs-link'); 
+links = Array.from(links).map(element => new TabLink(element)); 
+links[0].select(event);
